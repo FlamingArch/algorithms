5ba34246aa61d91894b093db9d8a56505636c7ba
Harsh Chaturvedi <16736449+FlamingArch@users.noreply.github.com>
2023-12-06 12:51:32 +0530

    Refactor, SinglyLinkedList InsertAt, tail Pointer

--- LinkedList.ts
+++ LinkedList.ts
@@ -1,13 +1,4 @@
-interface LinkedList<T> {
-  get length(): number; // ✅
-  insertAt(item: T, index: number): void;
-  remove(item: T): T | undefined;
-  removeAt(index: number): T | undefined;
-  append(item: T): void; // ✅
-  prepend(item: T): void;
-  get(index: number): T | undefined;
-  toString(): String;
-}
+import { LinkedList } from "./interfaces/index.ts";
 
 class ListNode<T> {
   value: T;
@@ -34,6 +25,14 @@ export class SinglyLinkedList<T> implements LinkedList<T> {
     return this.listLength;
   }
 
+  get headNode(): T | undefined {
+    return this.head?.value;
+  }
+
+  get tailNode(): T | undefined {
+    return this.tail?.value;
+  }
+
   append(item: T): void {
     if (this.head) {
       let current = this.head;
@@ -41,22 +40,58 @@ export class SinglyLinkedList<T> implements LinkedList<T> {
         current = current.next;
       }
       current.next = new ListNode(item);
+      this.tail = current.next;
       this.listLength += 1;
       return;
     }
     this.head = new ListNode(item);
+    this.tail = this.head;
     this.listLength += 1;
   }
 
   toString(): String {
     let current = this.head;
-    let string =
-      `List Length: ${this.length}\nList Values: ` + this.head?.value ??
-      "Empty List";
+    let string = "";
+
+    string += `Length: ${this.length}\n`;
+    string += `Head: ${this.headNode} \n`;
+    string += `Tail: ${this.tailNode}\n`;
+
+    string += `List Values: ` + this.head?.value ?? "Empty List";
     while (current?.next) {
       current = current.next;
       string += `, ${current.value}`;
     }
     return string;
+  }
+
+  insertAt(item: T, index: number): void {
+    if (index == 0) {
+      let newNode = new ListNode(item);
+      newNode.next = this.head;
+      this.head = newNode;
+      if (this.length == 0) this.tail = newNode;
+      this.listLength++;
+      return;
+    }
+
+    let current = this.head;
+    for (let i = 1; i < index; i++) {
+      if (current) {
+        current = current?.next;
+      } else {
+        throw new Error("Index Out Of Range!");
+      }
+    }
+
+    if (current) {
+      let newNode = new ListNode(item);
+      newNode.next = current.next;
+      current.next = newNode;
+      this.listLength++;
+      if (this.length - 1 == index) this.tail = newNode;
+    } else {
+      throw new Error("Index Out Of Range!");
+    }
   }
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import { SinglyLinkedList } from "./LinkedList.ts";
 
+let lineSeperator = "";
+
 let list = new SinglyLinkedList();
 list.append(2);
 list.append(4);
 list.append(6);
-console.log(list.toString());
+console.log(list.toString() + lineSeperator);
+list.insertAt(1, 0);
+list.insertAt(3, 2);
+list.insertAt(5, 4);
+console.log(list.toString() + lineSeperator);
